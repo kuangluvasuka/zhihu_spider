2491b0d48bd193fd8e52f5cacdf17b2f2e692bff
kuangluvasuka <lucifernine86@gmail.com>
2016-10-05 00:12:27 -0700

    first learning commit: urllib to download and re to parse

--- zhihu_spider.py
+++ zhihu_spider.py
@@ -1,1 +1,23 @@
 #-*- coding: UTF-8 -*-
+
+import sys
+import re
+import urllib
+from bs4 import BeautifulSoup
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+    
+def getImg(html):
+    imgre = re.compile(r'src="(.+?\.jpg)" pic_ext')
+    imglist = re.findall(imgre, html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl, '%s.jpg' % x)
+        x += 1
+
+if __name__ == "__main__":
+    html = getHtml("http://tieba.baidu.com/p/2460150866")
+    getImg(html)
