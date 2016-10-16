037034d3ff22a94bede1a2225ff59dbb58d603bf
kuangluvasuka <lucifernine86@gmail.com>
2016-10-15 17:06:50 -0700

    added my first doubanMovie spider

---
+++ doubanMovie.py
@@ -0,0 +1,63 @@
+#-*- coding: UTF-8 -*-
+
+import codecs
+import sys
+import random
+import requests
+from lxml import etree
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+headers = [
+    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'},
+    {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/601.6.17 (KHTML, like Gecko) Version/9.1.1 Safari/601.6.17'}
+    ]
+
+def movie_spider(movieTag):
+    """
+    """
+    root = "https://movie.douban.com/tag/%s" % movieTag
+    global headers
+    result = ''
+    html = requests.get(root, headers=random.choice(headers)).content
+    tree = etree.HTML(html.decode('utf-8'))
+    items = tree.xpath("//table/tr[@class='item']")
+    print len(items)
+    count = 1 
+    for item in items:
+        print count
+        itemURL = item.xpath("td/a[@class='nbg']/@href")[0].strip()
+        itemHTML = requests.get(itemURL, headers=random.choice(headers)).content
+        itemTree = etree.HTML(itemHTML.decode('utf-8'))
+        title = itemTree.xpath("//h1/span[@property='v:itemreviewed']/text()")[0].strip()
+        info = itemTree.xpath("//div[@class='subject clearfix']/div[@id='info']")[0]
+        director = info.xpath(".//a[@rel='v:directedBy']/text()")
+        scriptor = info.xpath("span")[1].xpath("span/a/text()")      # scriptor is not well formatted
+        actors = info.xpath(".//a[@rel='v:starring']/text()")
+        genre = info.xpath(".//span[@property='v:genre']/text()")
+        initDate = info.xpath(".//span[@property='v:initialReleaseDate']/text()")
+        rating = itemTree.xpath("//strong[@property='v:average']/text()")[0].strip()
+        count += 1
+        
+        result += '电影：' + title + '\n' \
+            + '导演：' + '/'.join(director[:]) + '\n' \
+            + '编剧：' + '/'.join(scriptor[:]) + '\n' \
+            + '演员：' + '/'.join(actors[:]) + '\n' \
+            + '类型：' + '/'.join(genre[:]) + '\n' \
+            + '上映日期：' + '/'.join(initDate[:]) + '\n' \
+            + '评分：' + rating + '\n' \
+            + '--' * 40 + '\n'
+           
+    
+
+        
+    print result
+
+
+
+
+if __name__ == "__main__":
+    tag = "宫崎骏"
+    movie_spider(tag)
+    
+
